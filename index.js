2391406e99f91a19fa8df24eac12168a08a62ae5
lmonty22 <lindsay.montgomery@olympian.org>
2020-04-18 15:35:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
 
 // Code your solution here
+
+let findMatching = (array, string) => array.filter(name => { 
+    return name.toUpperCase() === string.toUpperCase();
+ });
+
+let fuzzyMatch = (array, string) => array.filter(name => {
+    return name[0].toUpperCase() === string[0].toUpperCase();
+});
+
+// function fuzzyMatch(drivers, string){
+//     return drivers.filter(driver => {
+//       return driver.startsWith(string)
+//     })
+//   }
+
