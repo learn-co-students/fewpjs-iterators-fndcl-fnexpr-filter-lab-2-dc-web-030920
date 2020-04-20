a96f33e1e9110996f0d3d8b1ad7603f6f37bf8c7
bret-gibson <bret.gibson@gmail.com>
2020-04-20 06:20:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
 
 // Code your solution here
+const findMatching = (drivers, string) => {
+    return drivers.filter(driver => driver.toUpperCase() == string.toUpperCase())
+}
+
+const fuzzyMatch = (drivers, string) => {
+    return drivers.filter(driver => driver.slice(0, string.length).toUpperCase() === string.toUpperCase())
+}
